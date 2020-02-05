dbd7243d3b5f89ff86ced3c574d1e92a1d179ca9
Samuel Lucas <samuelborgeslucas@gmail.com>
2020-02-05 17:26:30 -0300

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
+# brew tap wix/brew
+# brew update
+# brew install applesimutils
+
+# # Change ios.sim.release to the detox configuration you want to run
+
+# echo "Building the project for Detox tests..."
+# npx detox build --configuration ios.sim.release
+
+# echo "Executing Detox tests..."
+# npx detox test --configuration ios.sim.release --cleanup
+
+echo "PRE BUILD STARTED"
+
+echo "iOS: Installing applesimutils..."
 brew tap wix/brew
-brew update
-brew install applesimutils
+brew install wix/brew/applesimutils
 
-# Change ios.sim.release to the detox configuration you want to run
+echo "Identifying selected node version..."
+node --version
 
-echo "Building the project for Detox tests..."
+echo "Installing detox cli..."
+npm install -g detox-cli
+
+echo "iOS: Building the project for Detox tests..."
 npx detox build --configuration ios.sim.release
 
-echo "Executing Detox tests..."
+echo "iOS: Cleaning cache for Detox tests..."
+npx clean-framework-cache && detox build-framework-cache
+
+echo "iOS: Executing Detox tests..."
 npx detox test --configuration ios.sim.release --cleanup
+
+echo "PRE BUILD COMPLETED"
