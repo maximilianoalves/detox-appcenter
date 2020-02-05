380a4cbbe1c64aa0b57c0b91b240e45f1705c0e7
Samuel Lucas <samuelborgeslucas@gmail.com>
2020-02-05 17:30:45 -0300

    Create appcenter-post-clone.sh

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/example/SomeProject
+
+# Example: Install App Center CLI
+npm install -g appcenter-cli
+
+echo "RUNNNNNNN"
